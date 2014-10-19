3317306650472e705b264cbe25851765e3ea3cd3
Ben Kirman <ben@kirman.org>
2014-10-19 22:59:44 +0100

    LibGDX 2d Scene scaffolding

--- nlfs-gdx/desktop/src/uk/ac/lincoln/games/nlfs/desktop/DesktopLauncher.java
+++ nlfs-gdx/desktop/src/uk/ac/lincoln/games/nlfs/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import uk.ac.lincoln.games.nlfs.NonLeague;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 640;
+		config.width = 360;
 		new LwjglApplication(new NonLeague(), config);
 	}
 }
