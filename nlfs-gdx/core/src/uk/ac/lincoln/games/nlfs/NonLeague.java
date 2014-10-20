f7dbc4a54f3af8541e9554b5d4d72cbc0eebda48
Ben Kirman <ben@kirman.org>
2014-10-20 16:34:33 +0100

    Messing around with skins

--- nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/NonLeague.java
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/NonLeague.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -27,49 +29,80 @@ public class NonLeague extends ApplicationAdapter {
 	Skin skin;
 	Stage stage;
 	
+	/**
+	 * Actor for background texture.
+	 * @author bkirman
+	 *
+	 */
+	public class Background extends Actor {
+		Texture bg = new Texture("bg_fill.png");
+		
+		public Background() {
+			super();
+			bg.setWrap(TextureWrap.Repeat,TextureWrap.Repeat);
+		}
+		@Override
+		public void draw(Batch batch, float alpha) {
+			batch.draw(bg, 0, 0,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());//fill
+		}
+	}
+	
+	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		stage = new Stage(new FitViewport(360,640));
+		
 		Gdx.input.setInputProcessor(stage);
+		
 		skin = new Skin();
 		// Generate a 1x1 white texture and store it in the skin named "white".
 		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
 		pixmap.setColor(Color.WHITE);
 		pixmap.fill();
+		
+		
+		
+		
 		skin.add("white", new Texture(pixmap));
 		// Store the default libgdx font under the name "default".
 		skin.add("default", new BitmapFont());
+		skin.add("button_up",new Texture("button_up.png"));
+		skin.add("button_down",new Texture("button_down.png"));
 		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
-		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-		textButtonStyle.font = skin.getFont("default");
-		skin.add("default", textButtonStyle);
+		TextButtonStyle button_style = new TextButtonStyle();
+		button_style.font = skin.getFont("default");
+		button_style.up = skin.newDrawable("button_up");
+		button_style.down = skin.newDrawable("button_down");
+		
+				
+		skin.add("default", button_style);
 		skin.add("default", new LabelStyle(new BitmapFont(),Color.WHITE));
 		// Create a table that fills the screen. Everything else will go inside this table.
 		Table table = new Table();
 		table.setFillParent(true);
+		table.setSkin(skin);
+		
+		
+		//build stage
+		stage.addActor(new Background());
 		stage.addActor(table);
 		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
 		final TextButton button = new TextButton("Click me!", skin);
-		table.add(button);
-		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+		table.add(button).width(200).height(40);
+		// Add a listener to the bu tton. ChangeListener is fired when the button's checked state changes, eg when clicked,
 		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
 		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
 		// revert the checked state.
 		button.addListener(new ChangeListener() {
-		public void changed (ChangeEvent event, Actor actor) {
-		System.out.println("Clicked! Is checked: " + button.isChecked());
-		button.setText("Good job!");
+			public void changed (ChangeEvent event, Actor actor) {
+			System.out.println("Clicked! Is checked: " + button.isChecked());
+			button.setText("Good job!"); 
 		}
 		});
-		// Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
-		//table.add(new Image(skin.newDrawable("white", Color.RED))).size(64);
+		
 		table.row();
-		table.add(new Label("Test",skin));
+		table.add(new Label("Test2",skin));
 	}
 
 	@Override
@@ -77,7 +110,11 @@ public class NonLeague extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		//stage.getBatch().begin();
+		//stage.getBatch().draw(bg, 0, 0,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		
 		stage.draw();
+		//stage.getBatch().end();
 	}
 	@Override
 	public void resize (int width, int height) {
