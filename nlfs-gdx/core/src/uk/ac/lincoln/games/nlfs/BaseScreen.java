1d87a0addb2125a399b89c8fd4d7d7752f0583b5
Ben Kirman <ben@kirman.org>
2014-10-31 17:09:37 +0000

    Starting screen classes

---
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/BaseScreen.java
@@ -0,0 +1,68 @@
+package uk.ac.lincoln.games.nlfs;
+
+import uk.ac.lincoln.games.nlfs.NonLeague.Background;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public abstract class BaseScreen implements Screen {
+	
+	protected final NonLeague game;
+    protected final Stage stage;
+    protected final Table table;
+
+    public BaseScreen(NonLeague game)
+    {
+        this.game = game;
+        this.stage = new Stage(game.viewport);
+        Gdx.input.setInputProcessor(stage);
+		table = new Table();
+		table.setFillParent(true);
+		table.setSkin(game.skin);
+		
+		stage.addActor(game.new Background());
+		stage.addActor(table);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor( 0f, 0f, 0f, 1f );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT );
+
+        // update and draw the stage actors
+        stage.act(delta);
+        stage.draw();
+    }
+    
+    @Override
+    public void resize(int width, int height) {
+    	stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void show() {        
+    }
+
+    @Override
+    public void hide() {
+        
+    }
+
+    @Override
+    public void pause() {
+ 
+    }
+
+    @Override
+    public void resume() {
+       
+    }
+
+    @Override
+    public void dispose() { //never called directly remember
+        stage.dispose();
+    }
+}
--- nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/PreMatch.java
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/PreMatch.java
@@ -1,10 +1,31 @@
 package uk.ac.lincoln.games.nlfs;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
 /**
  * Screen before match where players select the rituals they will use
  * @author bkirman
  *
  */
-public class PreMatch {
-
+public class PreMatch extends BaseScreen{
+	public PreMatch (NonLeague game) {
+		super(game);
+		
+		TextButton button = new TextButton("Go to Game", game.skin);
+		table.add(button).width(200).height(40);
+		table.row();
+		button.addListener(new ChangeListener() {
+			public void changed(ChangeEvent event, Actor actor) {
+		}
+		});
+	}
+	
+	@Override
+	public void render(float delta){
+		super.render(delta);
+	}
 }
