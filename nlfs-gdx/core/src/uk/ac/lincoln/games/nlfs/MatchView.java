a8740b1c0a1f914b5e5f71c1980d7b024b8abd0e
Ben Kirman <ben@kirman.org>
2015-05-01 16:10:33 +0100

    setting up android build env

--- nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/MatchView.java
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/MatchView.java
@@ -40,7 +40,7 @@ public class MatchView extends BaseScreen{
 	//private int mins_in_match;
 	private int current_minute, current_home, current_away;
 	public static float SIMULATION_S_PER_MIN = 0.4f;
-	private static boolean SKIP_MATCH = true;//debug setting skips slow match report
+	private static boolean SKIP_MATCH = false;//debug setting skips slow match report
 	private enum MatchState {PRE,H1,HT,H2,FT};
 	private MatchState current_state;
 	
