c321eb44bada4527f2b7cfaea0b25ddef149e785
Ben Kirman <ben@kirman.org>
2015-05-09 17:34:48 +0100

    Put temp icons into ritual selection

--- nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/PreMatch.java
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/PreMatch.java
@@ -1,5 +1,6 @@
 package uk.ac.lincoln.games.nlfs;
 
+
 import uk.ac.lincoln.games.nlfs.logic.GameState;
 import uk.ac.lincoln.games.nlfs.logic.Match;
 import uk.ac.lincoln.games.nlfs.ui.RitualSelector;
@@ -8,7 +9,6 @@ import uk.ac.lincoln.games.nlfs.ui.TeamLabel;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 /**
@@ -19,10 +19,79 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 public class PreMatch extends BaseScreen{
 	private Match match;
 	
-	private TeamLabel home_label,away_label;
-	private Label stadium_label,weather_label;
+	private TeamLabel home_label,away_label; 
+	private Label stadium_label;//,weather_label;
 	
 	private TextButton button;
+	
+	private static String[] drink_icons = {
+		"none",
+		"bovril",
+		"lager",
+		"bitter",
+		"brew",
+		"pop",
+		
+	};
+	private static String[] drink_names = {
+		"Nothing Special",
+		"Meaty Drink",
+		"Warm Lager",
+		"Flat Bitter",
+		"Milky Cuppa",
+		"Supermarket Pop",
+	};
+	
+	private static String[] food_icons = {
+		"none",
+		"bovril",
+		"lager",
+		"bitter",
+		"brew",
+		"pop",
+	};
+	private static String[] food_names = {
+		"Nothing Special",
+		"'Meat' Pie",
+		"Cheese Pie",
+		"Chip Pie",
+		"Curry Pie",
+		"Pie Barm",
+	};
+	
+	private static String[] clothing_icons = {
+		"none",
+		"bovril",
+		"lager",
+		"bitter",
+		"brew",
+		"pop",
+	};
+	private static String[] clothing_names = {
+		"Nothing Special",
+		"Team Shirt",
+		"Anorak",
+		"Scarf",
+		"Lucky Pants",
+		"Bare Chest",
+	};
+	private static String[] bringing_icons = {
+		"none",
+		"bovril",
+		"lager",
+		"bitter",
+		"brew",
+		"pop",
+	};
+	private static String[] bringing_names = {
+		"Nothing Special",
+		"Dog on String",
+		"Mardy Nephew",
+		"Vuvuzela",
+		"Stolen Brolly",
+		"Tuba",
+		
+	};
 	
 	public PreMatch (final NonLeague game) {
 		super(game);
@@ -30,7 +99,7 @@ public class PreMatch extends BaseScreen{
 		home_label = new TeamLabel(null,"teamname_bigger");
 		away_label = new TeamLabel(null,"teamname_bigger");
 		stadium_label = new Label("at [STADIUM]",Assets.skin);
-		weather_label = new Label("weather",Assets.skin);
+		//weather_label = new Label("weather",Assets.skin);
 		
 		table.add(home_label).expandX().fillX().left().colspan(2);
 		table.row();
@@ -38,22 +107,25 @@ public class PreMatch extends BaseScreen{
 		table.row();
 		table.add(away_label).expandX().fillX().left().colspan(2);
 		table.row();
-		table.add(weather_label).left();
-		table.add(stadium_label).expandX().right();
-		table.row().padBottom(5);
+		//table.add(weather_label).left();
+		table.add(stadium_label).expandX().colspan(2).center();
+		table.row().padBottom(15);
 		
-		RitualSelector clothes_ritual = new RitualSelector("Wearing");
-		RitualSelector food_ritual = new RitualSelector("Eating");
-		RitualSelector drink_ritual = new RitualSelector("Drinking");
+		RitualSelector clothes_ritual = new RitualSelector("Wearing",clothing_icons,clothing_names);
+		RitualSelector food_ritual = new RitualSelector("Eating",food_icons,food_names);
+		RitualSelector drink_ritual = new RitualSelector("Drinking",drink_icons,drink_names);
+		RitualSelector bring_ritual = new RitualSelector("Bringing",bringing_icons,bringing_names);
 		//clothes_ritual.validate();
 		table.add(clothes_ritual.getActor()).expandX().center().colspan(2);
-		table.row().padBottom(5);
+		table.row().padBottom(10);
 		table.add(food_ritual.getActor()).expandX().center().colspan(2);
-		table.row().padBottom(5);
+		table.row().padBottom(10);
 		table.add(drink_ritual.getActor()).expandX().center().colspan(2);
-		table.row();
+		table.row().padBottom(10);;
+		table.add(bring_ritual.getActor()).expandX().center().colspan(2);
+		table.row().padBottom(10);;
 		
-		
+		 
 		button = new TextButton("Go to Match", Assets.skin);	
 
 		table.add(button).width(480).height(85).colspan(2);
@@ -65,7 +137,6 @@ public class PreMatch extends BaseScreen{
             public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
             	game.changeScreen(game.matchview_screen);
 			}});
-		
 	}
 	
 	public void update() {
@@ -79,12 +150,10 @@ public class PreMatch extends BaseScreen{
 		if(match.away.colour_base==match.home.colour_base&&match.away.colour_primary==match.home.colour_primary) {
 			away_label.getStyle().background = Assets.skin.newDrawable("base",Assets.skin.getColor(match.away.colour_primary));
 			away_label.getStyle().fontColor = Assets.skin.getColor(match.away.colour_base);
-
 		} 
 		
-		
-		weather_label.setText(match.getWeather());
-		stadium_label.setText("at "+match.home.stadium);
+		//weather_label.setText(match.getWeather());
+		stadium_label.setText(match.getWeather()+" at "+match.home.stadium);
 	}
 	
 	@Override
--- nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/ui/RitualSelector.java
+++ nlfs-gdx/core/src/uk/ac/lincoln/games/nlfs/ui/RitualSelector.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 
 import uk.ac.lincoln.games.nlfs.Assets;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-//import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 /**
@@ -24,35 +26,53 @@ public class RitualSelector {
 	private Label selected_label;
 	private Table table;
 	
-	private ArrayList<TextButton> buttons;
+	private ArrayList<RitualButton> buttons;
 	private ScrollPane sp;
 	
-	public RitualSelector(String description) {
-		selected_label = new Label("Nothing Special",Assets.skin);
-		buttons = new ArrayList<TextButton>();
-		for(int i=1;i<8;i++) {//TODO real ones
-			buttons.add(new TextButton("TEST"+String.valueOf(i),Assets.skin,"ritual_button"));
+	public class RitualButton extends Button {
+		private String description;
+		private Texture ritual_icon;
+		
+		RitualButton(String icon, String name) {
+			super(Assets.skin,"ritual");
+			description = name;
+			ritual_icon = new Texture(Gdx.files.internal(icon+".png"));
+			add(new Image(ritual_icon));
+			addListener(new ChangeListener() {
+				public void changed(ChangeEvent event, Actor actor) {
+					selected_label.setText(description);
+				}});
 		}
+		
+	}
+	
+	
+	public RitualSelector(String description,String[] icons, String[] names) {
+		selected_label = new Label("Nothing Special",Assets.skin);
+		buttons = new ArrayList<RitualButton>();
 		ButtonGroup bg = new ButtonGroup();
 		HorizontalGroup hg = new HorizontalGroup();
-		for(TextButton b:buttons) {
-			hg.addActor(b);
-			b.addListener(new ChangeListener() {
-				public void changed(ChangeEvent event, Actor actor) {
-					selected_label.setText(((TextButton)actor).getText());
-			}
-			});
-			bg.add(b);
+		
+		for(int i=0;i<icons.length;i++) {
+			buttons.add(new RitualButton(icons[i],names[i]));
 		}
+		
+		for(RitualButton b:buttons) {
+			hg.addActor(b);
+			bg.add(b);	
+		}
+		
 		bg.setMaxCheckCount(1);
 		bg.setUncheckLast(true);
 		sp = new ScrollPane(hg);
 		table = new Table();
 		table.setBackground(Assets.skin.getDrawable("darken"));
-		table.add(sp).colspan(2);
-		table.row();
+		
 		table.add(new Label(description+":",Assets.skin)).left().padLeft(10);
 		table.add(selected_label).right().expandX().padRight(10);
+		table.row();
+		table.add(sp).colspan(2);
+		
 	}
 	
 	public Table getActor() {return table;}
